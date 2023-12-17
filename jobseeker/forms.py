3daca4b56d9875d9bf4de5c2cd1f54d7ec58c603
nikhil <nikhiljose59@gmail.com>
2023-12-17 20:30:44 +0530

    15th

--- jobseeker/forms.py
+++ jobseeker/forms.py
@@ -8,7 +8,7 @@ class RegistrationForm(UserCreationForm):
 
     class Meta:
         model=User
-        fields=["username","email","password1","password2"]
+        fields=["username","email","password1","password2","first_name"]
 
 class ProfileForm(forms.ModelForm):
 
